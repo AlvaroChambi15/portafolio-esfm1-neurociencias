1eb607593dd9d67bcddd7d5c9790bca741224098
AlvaroMC <alvaromch1510@gmail.com>
2022-09-18 09:09:10 -0400

    First Commit

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+// alert("HOLAA ESTO ES UNA ALERTA DESDE EL .JSON DEL PROYECTO!!!! :) ")
+
+// const nav = document.querySelector(".nav");
+
+// window.addEventListener('scroll', function(){
+//     nav.classList.toggle('activate', window.scrollY > 0)
+// })
+
+
+const nav = document.querySelector(".header-menu-wrapper");
+
+window.addEventListener('scroll', function(){
+    nav.classList.toggle('activate', window.scrollY > 0)
+})
